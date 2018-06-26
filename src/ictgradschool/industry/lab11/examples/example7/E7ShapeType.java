4d7f53bf0336b4b40d8808871e9669324d2ce601
Vita <vita.tsai@auckland.ac.nz>
2018-06-26 13:23:10 +1200

    Renamed lab files to new naming scheme

--- src/ictgradschool/industry/lab11/examples/example7/E7ShapeType.java
+++
@@ -1,9 +0,0 @@
-package ictgradschool.industry.lab11.examples.example7;
-
-/**
- * An enumeration of all the different types of shapes.
- */
-public enum E7ShapeType {
-
-    Square, Diamond, Triangle, Hourglass;
-}
---
+++ src/ictgradschool/industry/lab_swingi/examples/example7/E7Direction.java
@@ -0,0 +1,10 @@
+package ictgradschool.industry.lab_swingi.examples.example7;
+
+/**
+ * An enumeration of all the different directions a shape could travel.
+ */
+public enum E7Direction {
+
+    Up, Down, Left, Right;
+
+}
